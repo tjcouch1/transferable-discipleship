92add7c6d231f28a4d136620de6ce294e84c7e56
TJ Couch <tjcouch1@gmail.com>
2023-02-02 21:29:10 -0600

    Removed scroll bar visual on web

--- src/components/screens/HeaderWithButtons.tsx
+++ src/components/screens/HeaderWithButtons.tsx
@@ -1,41 +1,42 @@
 import { NativeStackScreenProps } from '@react-navigation/native-stack';
 import React from 'react';
-import { ScrollView, StyleSheet, View } from 'react-native';
+import { StyleSheet, View } from 'react-native';
 import { Header, HeaderData } from '../contents/Header';
 import { ScreenDataBase } from './Screens';
 import { getScreenData } from '../../services/ScreenService';
 import { ButtonList, ButtonListData } from '../contents/ButtonList';
+import TScrollView from '../TScrollView';
 
 /** The data that defines the HeaderWithButtons screen */
 export type HeaderWithButtonsData = {
-    type: 'HeaderWithButtons';
-    headerData: HeaderData;
-    buttonListData: ButtonListData;
+  type: 'HeaderWithButtons';
+  headerData: HeaderData;
+  buttonListData: ButtonListData;
 } & ScreenDataBase;
 
 /** Screen with a header and a list of buttons */
 export const HeaderWithButtons = ({
-    navigation,
-    route,
+  navigation,
+  route,
 }: NativeStackScreenProps<any>) => {
-    const { headerData, buttonListData } = getScreenData(
-        route.name,
-    ) as HeaderWithButtonsData;
+  const { headerData, buttonListData } = getScreenData(
+    route.name,
+  ) as HeaderWithButtonsData;
 
-    return (
-        <ScrollView contentInsetAdjustmentBehavior="automatic">
-            <View style={styles.layout}>
-                <Header {...headerData} />
-                <ButtonList {...buttonListData} navigation={navigation} />
-            </View>
-        </ScrollView>
-    );
+  return (
+    <TScrollView contentInsetAdjustmentBehavior="automatic">
+      <View style={styles.layout}>
+        <Header {...headerData} />
+        <ButtonList {...buttonListData} navigation={navigation} />
+      </View>
+    </TScrollView>
+  );
 };
 
 const styles = StyleSheet.create({
-    layout: {
-        flex: 1,
-        justifyContent: 'space-between',
-        alignItems: 'center',
-    },
+  layout: {
+    flex: 1,
+    justifyContent: 'space-between',
+    alignItems: 'center',
+  },
 });
